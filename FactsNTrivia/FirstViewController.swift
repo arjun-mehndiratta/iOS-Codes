9fd37f777f0769a6eeea9b62483b5a57f304828f
Arjun Mehndiratta <arjun@Arjuns-Mac-mini.local>
2017-10-18 10:54:40 +0530

    Initial Commit

---
+++ FactsNTrivia/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FactsNTrivia
+//
+//  Created by Arjun Mehndiratta on 18/10/17.
+//  Copyright © 2017 Arjun Mehndiratta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FactsNTrivia/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FactsNTrivia
+//
+//  Created by Arjun Mehndiratta on 18/10/17.
+//  Copyright © 2017 Arjun Mehndiratta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
